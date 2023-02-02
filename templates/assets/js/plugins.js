45369e516c238e40cc662498b36a5772c79055a2
longjuan <769022681@qq.com>
2023-02-02 04:53:14 +0000

    fix: tocbot error Squashed commit of the following:
    commit d4df51eae830d39c3076db3ebcfcf37d805bdc8b
    Author: longjuan <769022681@qq.com>
    Date:   Thu Feb 2 03:33:04 2023 +0000
    
        fix tocbot error
    
    commit e455381466282b245112b77d08f9e24657c265cd
    Author: longjuan <769022681@qq.com>
    Date:   Wed Feb 1 15:09:23 2023 +0000
    
        fix markdown tocbot error
    
    commit dd8a0d300f206a7700190da1afb645822be89931
    Author: longjuan <769022681@qq.com>
    Date:   Wed Feb 1 12:39:49 2023 +0000
    
        import tocbot
    
    commit 1c2adc82c5c1896c0cc595199035d0ff1fea7f37
    Author: longjuan <769022681@qq.com>
    Date:   Wed Feb 1 11:24:33 2023 +0000
    
        remove tocbot.min.js import

--- templates/assets/js/plugins.js
+++ templates/assets/js/plugins.js
@@ -8,9 +8,6 @@ tocbot.init({
     // Which headings to grab inside of the contentSelector element.
     // 生成那些标题的级别
     headingSelector: 'h1,h2,h3,h4,h5,h6',
-    linkClass: '',
-    listClass: '',
-    listItemClass: '',
     // For headings inside relative or absolute positioned containers within content.
     hasInnerContainers: true,
 });
